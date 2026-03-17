bd8c6c294873593f86270b0ad17a2dd086ebb11d
kami <darksideinfinity983@gmail.com>
2026-03-17 15:28:41 +0000

    modification de de cors

--- config/cors.php
+++ config/cors.php
@@ -13,8 +13,8 @@ return [
     // ✅ Origines autorisées (DEV + fallback .env)
     'allowed_origins' => array_filter([
         env('FRONTEND_URL'), // si défini dans .env
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
+        'http://localhost:5174',
+        'http://127.0.0.1:5174',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
     ]),
