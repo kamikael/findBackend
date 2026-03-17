d9607e53593da77d03f1457af363d54fff26f0d4
kami <darksideinfinity983@gmail.com>
2026-03-17 15:34:56 +0000

    modification de de cors

--- config/cors.php
+++ config/cors.php
@@ -15,6 +15,8 @@ return [
         env('FRONTEND_URL'), // si défini dans .env
         'http://localhost:5174',
         'http://127.0.0.1:5174',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
     ]),
